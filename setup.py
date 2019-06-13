f8c8ab50dc39f2cdddd0e3dcd348826d3d535cd6
dmillard <david@ironox.com>
2019-06-13 15:55:49 -0700

    Remove duplicated dependency

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='eigengdb',
-    version='1.0',
-    packages=find_packages(),
-    scripts=['bin/eigengdb_register_printers'],
-    install_requires=['numpy'],
-    author="David Millard",
-    author_email="dmillard@usc.edu",
-    description='GDB pretty printers for eigen types',
-    long_description=open('README.rst', 'r').read(),
-    license="MPL2",
-    keywords="eigen gdb",
-    url="https://github.com/dmillard/eigengdb",
-    project_urls={
-        "Source Code": "https://github.com/dmillard/eigengdb",
-        "Bug Tracker": "https://github.com/dmillard/eigengdb/issues",
-    },
-    install_requires=[
-        'numpy',
-    ])
+setup(name='eigengdb',
+      version='1.0',
+      packages=find_packages(),
+      scripts=['bin/eigengdb_register_printers'],
+      install_requires=['numpy'],
+      author="David Millard",
+      author_email="dmillard@usc.edu",
+      description='GDB pretty printers for eigen types',
+      long_description=open('README.rst', 'r').read(),
+      license="MPL2",
+      keywords="eigen gdb",
+      url="https://github.com/dmillard/eigengdb",
+      project_urls={
+          "Source Code": "https://github.com/dmillard/eigengdb",
+          "Bug Tracker": "https://github.com/dmillard/eigengdb/issues",
+      })
